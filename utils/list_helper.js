6301be5642603dc9a4466db3cca58195a8e7b30d
David López Albajara <dlalbajara@gmail.com>
2021-02-22 20:43:34 +0100

    Add: exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 'there are no blogs'
+  }
+
+  return blogs.length === 1
+    ? blogs[0].likes
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
